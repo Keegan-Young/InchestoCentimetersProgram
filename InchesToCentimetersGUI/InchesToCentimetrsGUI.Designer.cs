40a3d8aae2d42d8d1604d4872fcc630575614d9d
Keegan-Young <59021335+Keegan-Young@users.noreply.github.com>
2020-12-15 13:13:26 -0600

    Add files via upload

---
+++ InchesToCentimetersGUI/InchesToCentimetrsGUI.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace InchesToCentimetersGUI
+{
+    partial class InchesToCentimetrsGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InchesLabel = new System.Windows.Forms.Label();
+            this.CentLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CalcButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InchesLabel
+            // 
+            this.InchesLabel.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InchesLabel.Location = new System.Drawing.Point(51, 55);
+            this.InchesLabel.Name = "InchesLabel";
+            this.InchesLabel.Size = new System.Drawing.Size(100, 23);
+            this.InchesLabel.TabIndex = 0;
+            this.InchesLabel.Text = "Inches:";
+            this.InchesLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CentLabel
+            // 
+            this.CentLabel.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CentLabel.Location = new System.Drawing.Point(51, 106);
+            this.CentLabel.Name = "CentLabel";
+            this.CentLabel.Size = new System.Drawing.Size(100, 23);
+            this.CentLabel.TabIndex = 1;
+            this.CentLabel.Text = "Centimeters:";
+            this.CentLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(150, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // CalcButton
+            // 
+            this.CalcButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalcButton.Location = new System.Drawing.Point(54, 198);
+            this.CalcButton.Name = "CalcButton";
+            this.CalcButton.Size = new System.Drawing.Size(75, 23);
+            this.CalcButton.TabIndex = 4;
+            this.CalcButton.Text = "Calculate";
+            this.CalcButton.UseVisualStyleBackColor = true;
+            this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButton.Location = new System.Drawing.Point(166, 198);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 5;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(279, 198);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 6;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 238);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CalcButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CentLabel);
+            this.Controls.Add(this.InchesLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InchesLabel;
+        private System.Windows.Forms.Label CentLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button CalcButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ InchesToCentimetersGUI/InchesToCentimetrsGUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace InchesToCentimetersGUI
+{
+    public partial class InchesToCentimetrsGUI : Form
+    {
+        const double CENTIMETERSINANINCH = 2.54;
+
+        public InchesToCentimetrsGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            double inches = 0.0;
+            double centimeters = 0.0;
+
+            inches = Convert.ToDouble(textBox1.Text);
+            centimeters = inches * CENTIMETERSINANINCH;
+
+            textBox2.Text = centimeters.ToString("f2");
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox1.Focus();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show(
+                "Do you really want to exit?",
+                "Exit Program?",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
